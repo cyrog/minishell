a6315aebdba142f3b404e8780dfb8586a1d46f71
cgross <cgross@student.42lausanne.ch>
2023-05-22 16:59:00 +0200

    parsing path, need struct

---
+++ cmd_path.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_path.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgross <cgross@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 13:11:28 by cgross            #+#    #+#             */
+/*   Updated: 2023/05/22 16:46:05 by cgross           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	**get_all_path() //getenv path and splits it
+{
+	char **all_paths;
+   
+	all_paths = ft_split(getenv("PATH"), ':');
+	if (all_paths == NULL)
+		return (NULL);
+	else
+		return (all_paths);
+}
+
+char *get_right_path(char *cmd) //finds right path for cmd
+{
+	int     i;
+	char    *temp;
+	char	**all_paths;
+	char    *path;
+
+    i = -1;
+    all_paths = get_all_path();
+    while (all_paths && all_paths[++i])
+    {
+        temp = ft_strjoin(all_paths[i], "/");
+        path = ft_strjoin(temp, cmd);
+        if (access(path, X_OK != -1))
+        {
+            free(temp);
+			ft_free(all_paths);
+            return (path);
+        }
+        free(path);
+        free(temp);
+    }
+	ft_free(all_paths);
+    return (NULL);
+}
+
+int	main(void)
+{
+	printf("path %s", get_right_path("ls"));
+}
+
+
+// get_env->all_path function return value
+ 
+/*int main(void)
+{
+	char **split_path = get_all_path();
+	int	i = 0;
+	while (split_path[i])
+	{
+		printf("path[i]	[%d] [%s]\n", i, split_path[i]);
+		i++;
+	}
+}
+*/
---
+++ utils.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgross <cgross@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 13:43:58 by lobertho          #+#    #+#             */
+/*   Updated: 2023/05/17 13:49:45 by cgross           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
